ebf75b4dfb0266023df9275c11c31af6c46a4171
Franco Rodriguez <Franco.d.r1992@gmail.com>
2025-02-06 21:00:57 -0300

    CORS config 2

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,30 @@ import * as express from 'express';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['*'], 
+    origin: ['https://lmdr-backoffice.vercel.app', 'https://lmdr-backoffice.onrender.com'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'Origin'], 
     credentials: true,
   };
   
   app.enableCors(corsOptions);
 
   app.use('/upload', express.static('upload'));
+  
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'https://lmdr-backoffice.vercel.app');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With, Origin');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
   
   app.useGlobalPipes(
     new ValidationPipe({
